d330ec6a92d0821fa5e1aef9f67a6f4a5f561f2b
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2025-07-02 01:12:06 +0600

    create change password

--- apps/custom_auth/urls.py
+++ apps/custom_auth/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     #  path('',views.allUser),
      path('login/', views.login_view, name='login'),
+     path('change-password/', views.change_password,)
 ]
--- apps/custom_auth/views.py
+++ apps/custom_auth/views.py
@@ -1,11 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth.hashers import check_password, make_password
 from apps.users.models import User 
 from apps.users.serializers import UserSerializer
 from .serializers import UserLimitedSerializer
 from rest_framework_simplejwt.tokens import RefreshToken 
+from apps.core.middleware.customAuthGird import custom_auth_gird
     
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
@@ -40,3 +41,24 @@ def login_view(request):
     # serializer = UserSerializer(user)
     tokens = get_tokens_for_user(user)
     return Response({'message': 'Login successful',"user":serializer.data, 'tokens': tokens}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@custom_auth_gird(allowed_roles=['admin','doctor','patient'])
+def change_password(request):
+    user = request.user
+    old_password = request.data.get('old_password')
+    new_password = request.data.get('new_password')
+    
+    if not old_password or not new_password:
+        return Response({'detail': 'Both old and new passwords are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+    if not check_password(old_password, user.password):
+     return Response({'detail': 'Old password is incorrect'}, status=400)
+    
+    if check_password(new_password, user.password):
+        return Response({'detail': 'New password cannot be the same as the old password'}, status=400)
+        
+    user.password = make_password(new_password)
+    user.save()
+
+    return Response({'detail': 'Password changed successfully'}, status=status.HTTP_200_OK)
