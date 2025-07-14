7a661f91c7edb141119bccb581ff0b2122435484
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2025-07-14 15:33:14 +0600

    create get all doctor

--- apps/doctors/views.py
+++ apps/doctors/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Doctor
+from .serializers import DoctorSerializer
+from rest_framework.decorators import api_view
 
 # Create your views here.
-def doctors(request):
-     return HttpResponse("Welcome to MediTrack doctors")
+@api_view(['GET'])
+def getAllDoctor(request):
+     doctors = Doctor.objects.all()
+     serializer = DoctorSerializer(doctors, many=True)
+     # print(serializer.data,'data')
+     return Response(serializer.data, status=status.HTTP_200_OK)
+     # return HttpResponse("Welcome to MediTrack doctors")
