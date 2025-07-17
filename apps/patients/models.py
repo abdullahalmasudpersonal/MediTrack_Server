ad877e17d58d2d53c6c73b226db65bcca041d1bc
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2025-07-17 17:17:51 +0600

    migrate admin_photo/doctor_photo/patient_photo from photo

--- apps/patients/models.py
+++ apps/patients/models.py
@@ -19,7 +19,7 @@ class Patient(models.Model):
     name = models.CharField(max_length=30)
     age = models.PositiveIntegerField(blank=True, null=True)
     birthDate = models.DateField(blank=False, null=False)
-    patient_photo = models.ImageField(upload_to='patient_photo/', blank=True, null=True)
+    photo = models.ImageField(upload_to='photo/', blank=True, null=True)
     phone_number = models.CharField(validators=[phone_regex], max_length=11, blank=True,null=True)
     address = models.CharField(max_length=50)
     updated_at = models.DateTimeField(auto_now=True)    
