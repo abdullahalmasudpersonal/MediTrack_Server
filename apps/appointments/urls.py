7bf11140611d60204f0122bf4d96723c435c1ace
Abdullah Al Masud <abdullahalmasud772@gmail.com>
2025-11-10 01:14:49 +0600

    update get all & single patient

--- apps/appointments/urls.py
+++ apps/appointments/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('create-appointment/',views.create_appointment ),
-    path('all-appointment/',views.getAllAppointment ),
+    path('get-all-appointment/',views.getAllAppointment ),
     # path('single-appointment/<str:pk>/',views.getSingleAppointment ),
 ]
--- apps/patients/views.py
+++ apps/patients/views.py
@@ -4,18 +4,42 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import PatientSerializer
 from apps.core.middleware.customAuthGird import custom_auth_gird
+from apps.utils.response_helper import success_response, error_response
+
 
 # Create your views here.
-@api_view(['GET'])
-@custom_auth_gird(allowed_roles=['admin'])
+@api_view(["GET"])
+@custom_auth_gird(allowed_roles=["admin"])
 def getAllPatient(request):
-    patient = Patient.objects.filter(user__status='active',user__is_deleted=False)
-    serializer = PatientSerializer(patient, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    try:
+        patient = Patient.objects.all()
+        serializer = PatientSerializer(patient, many=True)
+        return success_response(
+            message="Get all patient successfully", data=serializer.data, code=200
+        )
 
-@api_view(['GET'])
-@custom_auth_gird(allowed_roles=['admin'])
-def getSinglePatient(request,pk):
-    patient = Patient.objects.get(user_id=pk,user__status='active',user__is_deleted=False)
-    serializer = PatientSerializer(patient)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    except Exception as e:
+        return error_response(
+            message="Failed to fetch get all patient", error=str(e), code=500
+        )
+
+
+@api_view(["GET"])
+@custom_auth_gird(allowed_roles=["admin"])
+def getSinglePatient(request, pk):
+    try:
+        patient = Patient.objects.all()
+        serializer = PatientSerializer(patient)
+        return success_response(
+            message="Get single patient successfully", data=serializer.data, code=200
+        )
+
+    except Patient.DoesNotExist:
+        return error_response(
+            message="Patient not found", error="Invalid patient ID", code=404
+        )
+
+    except Exception as e:
+        return error_response(
+            message="Failed to fetch get all patient", error=str(e), code=500
+        )
